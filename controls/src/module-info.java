64af57a98864d097eccb58c877f2cb5fb44d98e2
prodes13 <ciobotaru.florin@gmail.com>
2020-10-27 21:50:09 +0200

    events handlers

---
+++ controls/src/module-info.java
@@ -0,0 +1,6 @@
+module learnfx {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
--- controls/src/sample/Main.java
+++ controls/src/sample/Main.java
@@ -10,7 +10,6 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-//        Layouts here!
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Learning Java FX");
         primaryStage.setScene(new Scene(root, 800, 600));
